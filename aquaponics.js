ca6436e2042d0eb2e1fa591674a5b42d71aaa99f
GitHubYanis <yanis21p@gmail.com>
2020-12-26 16:17:35 -0800

    fishysuspiciousfish

--- aquaponics.js
+++ aquaponics.js
@@ -8,8 +8,32 @@ window.onload = function() {
     var img = new Image();
     img.src="simulation_background.png";
     img.onload = function(){       
-        ctx.drawImage(img, 0, 0);             
+        ctx.drawImage(img, 0, 0);
+    };
+
+    var fish = new Image();
+    fish.src="fishreal.png";
+    fish.onload = function(){       
+        ctx.drawImage(fish, 350, 450);      
     };   
+
+    var fish2 = new Image();
+    fish2.src="fishreal.png";
+    fish2.onload = function(){ 
+        ctx.save();
+        ctx.scale(-0.8,0.8);  
+        ctx.drawImage(fish2, -200, 500);
+        ctx.restore();
+    };
+
+    var fish3 = new Image();
+    fish3.src="fishreal.png";
+    fish3.onload = function(){ 
+        ctx.save();
+        ctx.scale(-1.1,1.1);  
+        ctx.drawImage(fish3, -200, 450);
+        ctx.restore();
+    };
 }
 
 function step1() {
